3c50e7f4776daaefcdd08ea8765571a8fae7a44b
Brian Gao <bgao@brandeis.edu>
2019-01-21 14:30:51 -0500

    add mongodb

--- controllers/calendarController.js
+++ controllers/calendarController.js
@@ -1,5 +1,22 @@
+var mongoose = require('mongoose');
+
+// Connect to the database
+mongoose.conntect('mongodb://dushen:Dachahu6493!@ds145639.mlab.com:45639/calendar');
+
+// schema
+var calendarSchema = new mongoose.Schema({
+   item: String 
+});
+
+var Calendar = mongoose.model('Calendar', calendarSchema);
+
 module.exports = function(app) {
     app.get('/', function(req, res) {
-        res.render('calendar');
+        // get data from mongodb and pass it to the view
+        Calendar.find({}, function(err, data) {
+            if (err) throw err;
+            res.render('calendar', {data: data});
+        });
+        
     })
 }
